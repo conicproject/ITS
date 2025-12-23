8ba3c078e5b6cd94b8a9474fe6d82e512fa7c566
mongkhon-inc <k.mongkhon9@gmail.com>
2025-12-23 09:23:58 +0700

    up

--- backend/app/src/services/auth.py
+++ backend/app/src/services/auth.py
@@ -7,7 +7,7 @@ from src.repositories.auth import AuthRepository
 
 SECRET_KEY = "your-secret-key"  # ควรเก็บใน .env
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_SECONDS = 60 * 60  # 60 นาที
+ACCESS_TOKEN_EXPIRE_SECONDS = 3600 * 60  # 60 นาที
 
 class AuthService:
     def __init__(self):
